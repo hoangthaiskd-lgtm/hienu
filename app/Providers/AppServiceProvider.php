d689e47d46ffd309fbd3b42859cd77a04d2d2754
Vương Quốc Hoàng Thái <vuongquochoangthai@gmail.com>
2026-08-24 20:05:55 +0700

    Authorization Gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,21 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        Gate::define('access-admin', fn($user) => $user->is_admin);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,24 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'can:access-admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dash', [ProfileController::class, 'showDashboard'])->name('profile.showDashboard');
 });
 
 require __DIR__.'/auth.php';
 
 // Route cho admin
-Route::prefix('admin')->name('admin.')->group(function() {
-    Route::get('login', [AdminAuthenticationController::class, 'showLogin'])->name('login');
-    Route::post('login', [AdminAuthenticationController::class, 'login'])->name('login.submit');
+// Route::prefix('admin')->name('admin.')->group(function() {
+//     Route::get('login', [AdminAuthenticationController::class, 'showLogin'])->name('login');
+//     Route::post('login', [AdminAuthenticationController::class, 'login'])->name('login.submit');
 
-    Route::middleware('admin.auth')->group(function() {
-        Route::get('dashboard', [AdminAuthenticationController::class, 'index'])->name('dashboard');
-        Route::get('logout', [AdminAuthenticationController::class, 'logout'])->name('logout');
-    });
-});
+//     Route::middleware('admin.auth')->group(function() {
+//         Route::get('dashboard', [AdminAuthenticationController::class, 'index'])->name('dashboard');
+//         Route::get('logout', [AdminAuthenticationController::class, 'logout'])->name('logout');
+//     });
+// });
